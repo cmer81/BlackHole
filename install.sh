f29845a7d4e0c57f100adbcc29dfdd0b8629e75c
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2013-09-09 22:29:54 -0300

    Modify install.sh

--- install.sh
+++ install.sh
@@ -26,11 +26,12 @@ then
     echo "READ: The installer wont install MySQLdb, because maybe you want to use some other engine. If you want to use Mysql, Install MySQLdb!!"
     echo "Installing app in $BLACKHOLE_PATH ..."
     mkdir $BLACKHOLE_PATH
-    groupadd blackhole
     cp -rf apache $BLACKHOLE_PATH
     cp -rf blackhole $BLACKHOLE_PATH
     cp launcher/* $BLACKHOLE_PATH
-	echo "Next steps..."
+    echo "Creating group ..."
+    groupadd blackhole
+	echo "Next steps ..."
 	echo "1. Create the database (name: blackhole) and a user (whatever you like)."
 	echo "2. Add the user/password you've created to $BLACKHOLE_PATH/blackhole/black_hole/settings.py in the DATABASES section"
 	echo "3. Create the tables with: $BLACKHOLE_PATH/blackhole/manage.py syncdb"
@@ -38,7 +39,7 @@ then
 	echo "5. Set the path where you want to save the session logs (make shure that the group 'blackhole' has write permissions!!!) in $BLACKHOLE_PATH/blackhole.config"
 	echo "6. Configure the web server. If you use apache you can use this example ($BLACKHOLE_PATH/apache/blackhole.conf.example), is ready to use. Remember to enable the port 8080."
     echo "7. That's all. Enter to the web (http://x.x.x.x:8080/blackhole/index/) and create all the configurations!!"
-	echo "8. Remember to set $BLACKHOLE_PATH/blackhole_launcher.py as the shell for all the users."
+	echo "8. Remember to set $BLACKHOLE_PATH/blackhole_launcher.py as the shell for all the users. And set 'blackhole' as their group"
 else
     echo "You need pip in you $PATH"
     exit 1
